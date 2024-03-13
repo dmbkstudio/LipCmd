5cc881d10544502e1566ac2d362253f2e8b4102c
diamondblock <62496819+dmblock@users.noreply.github.com>
2024-03-13 12:25:44 +0800

    Update fetchmetadata.sh

--- script/fetchmetadata.sh
+++ script/fetchmetadata.sh
@@ -1,43 +1,25 @@
-curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadatagitea.litebds.com%2FLiteLDev%2Flegacy-script-engine-lua.json> ./.lip/metadata/gitea.litebds.com%2FLiteLDev%2Flegacy-script-engine-lua.json
+curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/gitea.litebds.com%252FLiteLDev%252Flegacy-script-engine-lua.json > ./.lip/metadata/gitea.litebds.com%2FLiteLDev%2Flegacy-script-engine-lua.json
 
-curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/gitea.litebds.com%2FLiteLDev%2Flegacy-script-engine-quickjs.json > ./.lip/metadata/gitea.litebds.com%2FLiteLDev%2Flegacy-script-engine-quickjs.json
+curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/gitea.litebds.com%252FLiteLDev%252Flegacy-script-engine-quickjs.json > ./.lip/metadata/gitea.litebds.com%2FLiteLDev%2Flegacy-script-engine-quickjs.json
 
-curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%2FLiteLDev%2Fbds.json > ./.lip/metadata/github.com%2FLiteLDev%2Fbds.json
+curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%252FLiteLDev%252FCrashLogger.json > ./.lip/metadata/github.com%2FLiteLDev%2FCrashLogger.json
 
-curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%2FLiteLDev%2Fbdsdown.json > ./.lip/metadata/github.com%2FLiteLDev%2Fbdsdown.json
+curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%252FLiteLDev%252FLegacyMoney.json > ./.lip/metadata/github.com%2FLiteLDev%2FLegacyMoney.json
 
-curl -s https://mirror.ghproxy.com//https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%2FLiteLDev%2Fbdsdown-legacy.json > ./.lip/metadata/github.com%2FLiteLDev%2Fbdsdown-legacy.json
+curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%252FLiteLDev%252FLegacyParticleAPI.json > ./.lip/metadata/github.com%2FLiteLDev%2FLegacyParticleAPI.json
 
-curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%2FLiteLDev%2FCrashLogger.json > ./.lip/metadata/github.com%2FLiteLDev%2FCrashLogger.json
+curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%252FLiteLDev%252FLegacyRemoteCall.json > ./.lip/metadata/github.com%2FLiteLDev%2FLegacyRemoteCall.json
 
-curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%2FLiteLDev%2FLegacyRemoteCall.json > ./.lip/metadata/github.com%2FLiteLDev%2FLegacyRemoteCall.json
+curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%252FLiteLDev%252FLegacyScriptEngine.json > ./.lip/metadata/github.com%2FLiteLDev%2FLegacyScriptEngine.json
 
-curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%2FLiteLDev%2FLegacyScriptEngine.json > ./.lip/metadata/github.com%2FLiteLDev%2FLegacyScriptEngine.json
+curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%252FLiteLDev%252FLeviLamina.json > ./.lip/metadata/github.com%2FLiteLDev%2FLeviLamina.json
 
-curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%2FLiteLDev%2FLeviLamina.json > ./.lip/metadata/github.com%2FLiteLDev%2FLeviLamina.json
+curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%252FLiteLDev%252FPeEditor.json > ./.lip/metadata/github.com%2FLiteLDev%2FPeEditor.json
 
-curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%2FLiteLDev%2Flevilamina-loc.json > ./.lip/metadata/github.com%2FLiteLDev%2Flevilamina-loc.json
+curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%252FLiteLDev%252FPreLoader.json > ./.lip/metadata/github.com%2FLiteLDev%2FPreLoader.json
 
-curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%2FLiteLDev%2FPeEditor.json > ./.lip/metadata/github.com%2FLiteLDev%2FPeEditor.json
+curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%252FLiteLDev%252Fbds.json > ./.lip/metadata/github.com%2FLiteLDev%2Fbds.json
 
-curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%2FLiteLDev%2FPreLoader.json > ./.lip/metadata/github.com%2FLiteLDev%2FPreLoader.json
+curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%252FLiteLDev%252Fbdsdown-legacy.json > ./.lip/metadata/github.com%2FLiteLDev%2Fbdsdown-legacy.json
 
-echo "[DONE]"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+curl -s https://mirror.ghproxy.com/https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/github.com%252FLiteLDev%252Flevilamina-loc.json > ./.lip/metadata/github.com%2FLiteLDev%2Flevilamina-loc.json
