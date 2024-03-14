e1e67312b79a99a82dce88b383a0c1d708652a67
diamondblock <62496819+dmblock@users.noreply.github.com>
2024-03-14 05:52:36 +0000

    test

--- script/fetchmetadata.sh
+++ script/fetchmetadata.sh
@@ -1,4 +1,6 @@
-github_proxy="https://mirror.ghproxy.com/"
+export json_file="./plugins/Lipcmd/config/lipcmd.json"
+export github_proxy=$(grep -Po '"githubProxy":.*?[^\\]",' "$json_file" | sed 's/"githubProxy": "\(.*\)",/\1/')
+
 wget -qO- ${github_proxy}https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/gitea.litebds.com%252FLiteLDev%252Flegacy-script-engine-lua.json > ./.lip/metadata/gitea.litebds.com%2FLiteLDev%2Flegacy-script-engine-lua.json
 
 wget -qO- ${github_proxy}https://raw.githubusercontent.com/dmbkstudio/LipCmd/main/metadata/gitea.litebds.com%252FLiteLDev%252Flegacy-script-engine-quickjs.json > ./.lip/metadata/gitea.litebds.com%2FLiteLDev%2Flegacy-script-engine-quickjs.json
